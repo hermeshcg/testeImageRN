cffbc92065420431261f63787818ddfafaab3d54
hermeshcg <hermescaretta@gmail.com>
2021-11-29 15:43:03 -0300

    acho que deu bom

--- App.js
+++ App.js
@@ -6,91 +6,75 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
+  Text,
   View,
 } from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {SliderBox} from 'react-native-image-slider-box';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+let images = [
+  'https://fotoprod2020.blob.core.windows.net/fotosprod/10004.JPG',
+  'https://fotoprod2020.blob.core.windows.net/fotosprod/1.JPG',
+  'https://fotoprod2020.blob.core.windows.net/fotosprod/10004.JPG',
+  'https://fotoprod2020.blob.core.windows.net/fotosprod/1.JPG',
+];
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
+const App = () => {
+  const [width, setWidth] = useState();
+  function onLayout(e) {
+    setWidth(e.nativeEvent.layout.width);
+  }
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  console.log('widt??', width);
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={styles.cardContainer}>
+        <View style={styles.containerImg} onLayout={onLayout}>
+          <SliderBox
+            images={images}
+            parentWidth={width}
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+          />
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  containerImg: {
+    width: '100%',
+    height: 200,
+  },
+  imgProduto: {
+    marginBottom: 'auto',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  cardContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '45%',
+    height: 400,
+    marginHorizontal: 10,
+    marginVertical: 16,
+    paddingBottom: 4,
+    backgroundColor: '#F5FCFF',
+    borderBottomRightRadius: 8,
+    borderBottomLeftRadius: 8,
+    elevation: 6,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
